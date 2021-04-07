d146ef8b09716773dd2e579e01229761653ecde3
Sriraj Bura <sriraj.bura16@gmail.com>
2021-04-06 23:27:29 -0400

    implemented get_move instance method

--- player.rb
+++ player.rb
@@ -1,5 +1,11 @@
 class Player
-  def initialize(name)
+  def initialize(name, symbol)
     @name = name
+    @symbol = symbol
+  end
+
+  def get_move
+    puts 'Player #{@symbol}, enter your input:'
+    gets.chomp
   end
 end
