3cdda4d6b14059d78de78d21bfbe2c18a63e91be
Sriraj Bura <sriraj.bura16@gmail.com>
2021-04-06 23:55:08 -0400

    implemented render_board instance method

--- board.rb
+++ board.rb
@@ -2,4 +2,15 @@ class Board
   def initialize
     @board = Array.new(3) { Array.new(3) }
   end
+
+  def render_board
+    @board.each_with_index do |row, row_idx|
+      row.each_with_index do |cell, col_idx|
+        print cell == nil ? (col_idx + 1) + (3 * row_idx) : cell
+        print ' | '  unless col_idx == 2
+      end
+      puts "\n- - - - -" unless row_idx == 2
+    end
+  end
+
 end
